3217e8ea896e34d9be5d8fcb73d580066c908428
fuRan <NgeKaworu@gmail.com>
2020-12-22 07:51:18 +0800

    node 初体验

---
+++ app.js
@@ -0,0 +1,13 @@
+const chalk = require("chalk");
+
+console.log(chalk.yellow("hi"));
+
+const ProgressBar = require("progress");
+
+const bar = new ProgressBar(":bar", { total: 10 });
+const timer = setInterval(() => {
+  bar.tick();
+  if (bar.complete) {
+    clearInterval(timer);
+  }
+}, 100);
